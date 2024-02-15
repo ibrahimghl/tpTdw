0d5bc5eeca58b6ba5f39f770b4070f47f9c106ad
ibrahimghl <i.ghoual@esi-sba.dz>
2024-02-15 21:17:30 +0100

    i did some changes

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,26 +1,12 @@
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Login</th>
-        <th>Password</th>
-        <th colspan="2">Action</th>
-    </tr>
-
-    <?php foreach ($users as $user): ?>
+modifief
     <tr>
         <td>
-            <?php echo  $user->nom ?>
+          
         </td>
         <td>
-            <?php echo  $user->age ?>
+           
         </td>
-        <td>
-            <form action="/deleteUser" method="get">
-                <input type="hidden" name="_token" value=<?php echo csrf_token(); ?> />
-                <input type="hidden" name="id" value="<?php echo $user->id ?>">
-                <button type="submit">Delete</button>
-            </form>
-        </td>
+      
         <td>
             <form action="/updateUserForm" method="get">
                 <input type="hidden" name="_token" value=<?php echo csrf_token(); ?> />
@@ -29,7 +15,7 @@
             </form>
         </td>
     </tr>
-    <?php endforeach;  ?>
+    
 </table>
 <form action="/createUser" method="get">
     <input type="submit" value="Create User" />
